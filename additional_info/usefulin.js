07adf0ae34ebd15b2b184f2e6d48e07922a984be
Zak khan <zakaria2012@gmail.com>
2023-02-02 10:51:02 +0000

    wrote the classes for all for requirements

---
+++ additional_info/usefulin.js
@@ -0,0 +1,8 @@
+/* Git: */
+        // echo "# Team-Profile-Generator" >> README.md
+        // git init
+        // git add README.md
+        // git commit -m "first commit"
+        // git branch -M main
+        // git remote add origin git@github.com:Zakaria1986/Team-Profile-Generator.git
+        // git push -u origin main
--- index.js
+++ index.js
@@ -13,3 +13,8 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+
+// webpage that displays my team's basic info so that a user have quick access to their emails and GitHub profiles.
+
+    // Create a command-line application that accepts user input using the provided starter code.
+    //  * Create classes for each team member provided and export them. The tests for these classes (in the `_tests_` directory) must ALL pass.     
