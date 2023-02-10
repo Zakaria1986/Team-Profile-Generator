7495a2afabe83ab5b90687e89621bf7345cec8b3
Zak khan <zakaria2012@gmail.com>
2023-02-10 13:14:33 +0000

    solving issue

--- additional_info/usefulin.js
+++ additional_info/usefulin.js
@@ -109,6 +109,86 @@ Manager Test Info:
     school: 'BGTC'
   }
 ]
+    
+
+
+
+  
+  #    Auto generate README.md file from console.   <a name="title-0"></a>
+  ##  Description <a name="description-1"></a>
+
      
 
+This porject is to create an app that would take an Employee, managers, engineers and intern information through node.js console, then parse that into renderable and traverse the information and design a humanly readable cards which then create an index.html card.  
+   
+
+## Table of Contents
+
+   
+1. [Description](#description-1)
+   
+2. [Installation](#instalation-2)
+   
+3. [Usage info](#usage-3)
+   
+4. [License](#license-4)
+   
+5. [Contributors](#contributor-5)
+   
+6. [Tester info](#tests-6)
+   
+7. [Further Q&A](#FurtherQA)
+   
+8. [GitHub Link](#GitProfileName)
+   
+   ## Instalation <a name="instalation-2"></a>
+
+      
+
+To run this app, make sure you have node installed on your machine then go to you project root folder then run: 1. npm init, 2. Install inquirer, and fs. 
+
+   ##  Usage<a name="usage-3"></a>
+
+      
+
+To run the app enter node index.html into node console, then you will be prompted with four option i.e. Manager, Engineer, Intern and Exist building the team. Depending on your require at that with will promp you with series of question which you must enter some value to proceed to next question or finish. 
+
+      
+
+[Walk through link:](https://www.youtube.com/watch?v=R_KmtZ3Xvx4) 
+
+   ##  License <a name="license-4"></a>
+
+      
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+   ##  Contributor <a name="contributor-5"></a>
+
+      
+
+If you would like to contribute to this project, please get in touch on [email](mailto:zakaria.khan@zaksweb.co.uk)
+
+   ##  Tests <a name="tests-6"></a>
+
+      
+
+If you would like to test and contribute to this app, please download a copy of the repo and let me know the bug or anythings. Thank you!
+
+   ##  Further Q&A <a name="FurtherQA"></a>
+
+      
+
+If you have any further question, please get in touch on: [email](zakaria.khan@zaksweb.co.uk)
+
+   ##  Git Profile Name <a name="GitProfileName"></a>
+
+      
+
+[Click to see git repository](https://github.com/Zakaria1986)
+   
+
+
 */
+
+
